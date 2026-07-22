70ca2fe2666b826521231887f79d4573f8b0e5c0
houqingxin <houqingxin@xender.com>
2026-07-22 13:27:07 +0800

    chore: bump version to 1.2.50 for deployment

--- android/lib/main.dart
+++ android/lib/main.dart
@@ -11,7 +11,7 @@ import 'views/home_view.dart';
 import 'services/localization_service.dart';
 import 'services/theme_service.dart';
 
-const String appVersion = '1.2.49';
+const String appVersion = '1.2.50';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
