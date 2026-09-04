1cd5fd9ccc453bda340a24a87f16c4eb6ef7e318
houqingxin <houqingxin@xender.com>
2026-09-04 08:46:08 +0800

    chore: bump version to 3.0.13 & release

--- android/lib/main.dart
+++ android/lib/main.dart
@@ -13,7 +13,7 @@ import 'services/localization_service.dart';
 import 'services/theme_service.dart';
 import 'services/analytics_service.dart';
 
-const String appVersion = '3.0.12';
+const String appVersion = '3.0.13';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
