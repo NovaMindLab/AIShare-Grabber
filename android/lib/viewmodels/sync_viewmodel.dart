95ab92f7bf9739fc021c18b2fab9973a4584b6e9
houqingxin <houqingxin@xender.com>
2026-09-16 15:35:44 +0800

    chore: bump version to 4.1.1 & prepare release

--- android/lib/viewmodels/sync_viewmodel.dart
+++ android/lib/viewmodels/sync_viewmodel.dart
@@ -42,7 +42,7 @@ enum TransferStatus {
 }
 
 class SyncViewModel extends ChangeNotifier {
-  static const String appVersion = '4.1.0';
+  static const String appVersion = '4.1.1';
   List<Map<String, dynamic>> discoveredPCs = [];
   Timer? _discoveryTimer;
   String? _mobileName;
