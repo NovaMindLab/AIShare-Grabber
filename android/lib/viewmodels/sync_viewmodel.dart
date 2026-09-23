6bdc4925c5dbd617bc519f85b6101b8d8bc9f57b
houqingxin <houqingxin@xender.com>
2026-09-23 15:57:47 +0800

    chore: bump version to v4.5.7 for automated cloud release

--- android/lib/viewmodels/sync_viewmodel.dart
+++ android/lib/viewmodels/sync_viewmodel.dart
@@ -42,7 +42,7 @@ enum TransferStatus {
 }
 
 class SyncViewModel extends ChangeNotifier {
-  static const String appVersion = '4.5.6';
+  static const String appVersion = '4.5.7';
   List<Map<String, dynamic>> discoveredPCs = [];
   Timer? _discoveryTimer;
   String? _mobileName;
